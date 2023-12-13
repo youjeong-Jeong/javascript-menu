7f7ec9055c5a4002fe6096b5913848acee077339
youjeong Jeong <wjddbwjd1205@naver.com>
2023-12-13 21:09:57 +0900

    feat: 각 코치가 못먹는 메뉴 입력 받기

--- src/ERROR.js
+++ src/ERROR.js
@@ -1,6 +1,7 @@
 const ERROR = Object.freeze({
   EMPTY: '[ERROR] 공백 입력입니다',
   INVALIDATE_COACH: '[ERROR] 올바르지 않은 코치 이름 입력입니다.',
+  INVALIDATE_MENU: '[ERROR] 올바르지 않은 못먹는 메뉴 입력입니다.',
 
 })
 
--- src/RecommendMenu.js
+++ src/RecommendMenu.js
@@ -1,28 +1,106 @@
 import InputView from "./InputView.js";
 import OutputView from "./OutputView.js";
 import ERROR from "./ERROR.js";
+import MENU from "./MENU.js";
 
 class RecommendMenu {
+  #coachs;
+  #coachsInedibleMenus;
 
-  constructor() {}
+  constructor() {
+    this.#coachsInedibleMenus = [];
+  }
 
   async start() {
     OutputView.printStartMessage();
-    const coachs = await this.userCoachInput();
+    this.#coachs = await this.userCoachInput();
+    this.userInedibleMenuInput();
   }
 
   async userCoachInput() {
-    while(true) {
+    while (true) {
       try {
         const inputCoach = await InputView.readCoach();
         this.validateIsEmpty(inputCoach);
         const coachs = inputCoach.split(',');
         this.validateCoach(coachs);
         return coachs;
-      } catch(error) {
+      } catch (error) {
         OutputView.printError(error);
       }
     }
+  }
+
+  async userInedibleMenuInput() {
+    const coachIterator = this.#coachs[Symbol.iterator]();
+    let result = coachIterator.next();
+
+    while (!result.done) {
+      await this.inedibleMenuInput(result.value);
+      result = coachIterator.next();
+    }
+  }
+
+  async inedibleMenuInput(coach) {
+    while (true) {
+      try {
+        const inputInedibleMenu = await InputView.readInedibleMenu(coach);
+        return this.validateMenuInput(inputInedibleMenu);       
+        //console.log(this.#coachsInedibleMenus);
+      } catch (error) {
+        OutputView.printError(error);
+      }
+    }
+  }
+
+  validateMenuInput(input) {
+    if(this.validateMenuIsEmpty(input)) return;   
+    const inedibleMenu = input.split(',');
+    this.validateMenuCount(inedibleMenu); // 2개이상인지 확인
+    this.validateMenu(inedibleMenu);
+
+    console.log(this.#coachsInedibleMenus);
+  }
+
+  validateNotInMenuList(menu) {
+    if (!MENU.ALL_MENUS.includes(menu)) {
+      throw new Error(ERROR.INVALIDATE_MENU);
+    }
+  }
+
+  validateMenuCount(menus) {
+    if (menus.length > 2) {
+      throw new Error(ERROR.INVALIDATE_MENU);
+    }
+  }
+
+  validateMenuIsEmpty(menu) {
+    if (!menu.trim()) {
+      this.#coachsInedibleMenus.push([]);
+      return true;
+    }
+    return false;
+  }
+
+  validateMenu(menus) {
+    if (menus.length === 1) {
+      this.validateInedibleMenu(menus[0]);
+      this.#coachsInedibleMenus.push([menus]);
+      return;
+    } 
+    if (menus.length === 2) {
+      console.log(menus);
+      menus.forEach(menu => {
+        this.validateIsEmpty(menu);
+        this.validateInedibleMenu(menu);
+      });
+      this.#coachsInedibleMenus.push([menus]);
+    }
+  }
+
+  validateInedibleMenu(menu) {
+    const menuName = this.removeWhitespace(menu);
+    this.validateNotInMenuList(menuName);
   }
 
   removeWhitespace(input) {
@@ -36,20 +114,20 @@ class RecommendMenu {
   }
 
   validateNameLength(name) {
-    if(name.length < 2 || name.length > 4) {
+    if (name.length < 2 || name.length > 4) {
       throw new Error(ERROR.INVALIDATE_COACH);
     }
   }
 
   validateCoachLength(coachs) {
-    if(coachs.length < 2 || coachs.length > 5) {
+    if (coachs.length < 2 || coachs.length > 5) {
       throw new Error(ERROR.INVALIDATE_COACH);
     }
   }
 
   validateCoach(coachs) {
     this.validateCoachLength(coachs);
-    coachs.forEach( coach => {
+    coachs.forEach(coach => {
       this.validateIsEmpty(coach);
       this.removeWhitespace(coach);
       this.validateNameLength(coach);
